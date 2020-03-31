911ad673d9ce1497bc3434e51cf383ff28d75d27
Dávid Csejtei <csejteidavid90@gmail.com>
2020-03-31 11:49:13 +0200

    Fix menu toggler size

--- src/content/components/Menu.js
+++ src/content/components/Menu.js
@@ -59,11 +59,11 @@ class MyMenu extends Component {
                             key="sub1"
                             title={
                                 <span>
-                                    <MailOutlined/>
-                                    <span>Navigation One</span>
+                                    <SettingOutlined />
+                                    <span>Settings</span>
                                 </span>
                             }>
-                            <Menu.Item key="1">Option 1</Menu.Item>
+                            <Menu.Item key="1">Roles and capabilities</Menu.Item>
                             <Menu.Item key="2">Option 2</Menu.Item>
                             <Menu.Item key="3">Option 3</Menu.Item>
                             <Menu.Item key="4">Option 4</Menu.Item>
@@ -82,19 +82,6 @@ class MyMenu extends Component {
                                 <Menu.Item key="7">Option 7</Menu.Item>
                                 <Menu.Item key="8">Option 8</Menu.Item>
                             </SubMenu>
-                        </SubMenu>
-                        <SubMenu
-                            key="sub4"
-                            title={
-                                <span>
-                                  <SettingOutlined />
-                                  <span>Navigation Three</span>
-                                </span>
-                            }>
-                            <Menu.Item key="9">Option 9</Menu.Item>
-                            <Menu.Item key="10">Option 10</Menu.Item>
-                            <Menu.Item key="11">Option 11</Menu.Item>
-                            <Menu.Item key="12">Option 12</Menu.Item>
                         </SubMenu>
                     </Menu>
                 </Sider>
